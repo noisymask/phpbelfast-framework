749222ffbd7cb0b0480ff519f7264c2d6288e788
noisymask <thomk@Thoms-MBP.local>
2014-09-06 00:15:31 +0100

    Menu renderer provider class

---
+++ PhpBelfast/Menu/RendererProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace PhpBelfast\Menu;
+
+use Knp\Menu\Renderer\RendererProviderInterface;
+
+class RendererProvider implements RendererProviderInterface {
+
+    /**
+     * @var \Slim\Helper\Set
+     */
+    private $container;
+
+    /**
+     * @var string
+     */
+    private $defaultRenderer;
+
+    /**
+     * @var array
+     */
+    private $rendererIds;
+
+    /**
+     * @param \Slim\Helper\Set $container
+     * @param $defaultRenderer
+     * @param array $rendererIds
+     */
+    public function __construct(\Slim\Helper\Set $container, $defaultRenderer, array $rendererIds)
+    {
+        $this->container = $container;
+        $this->rendererIds = $rendererIds;
+        $this->defaultRenderer = $defaultRenderer;
+    }
+
+    /**
+     * @param null $name
+     * @return \Knp\Menu\Renderer\RendererInterface
+     * @throws \InvalidArgumentException
+     */
+    public function get($name = null)
+    {
+        if (null === $name) {
+            $name = $this->defaultRenderer;
+        }
+
+        if (!isset($this->rendererIds[$name])) {
+            throw new \InvalidArgumentException(sprintf('The renderer "%s" is not defined.', $name));
+        }
+
+        return $this->container[$this->rendererIds[$name]];
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function has($name)
+    {
+        return isset($this->rendererIds[$name]);
+    }
+
+} 
